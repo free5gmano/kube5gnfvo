b62cabcbced11c5d41e7e201568ae5e95782656a
yanyan8566 <s1810832012@gms.nutc.edu.tw>
2020-11-07 06:45:48 +0000

    fix bug

--- utils/process_package/process_vnf_instance.py
+++ utils/process_package/process_vnf_instance.py
@@ -137,7 +137,7 @@ class ProcessVNFInstance(BaseProcess):
 
             rate, network_name_list = self._process_network(net_list, vdu, max_instances=max_instances)
             vdu_info['network_name'] = network_name_list
-            self.process_onos_sf(rate)
+            # self.process_onos_sf(rate)
 
             # update api
             if 'replicas' in kwargs and kwargs['replicas']:
