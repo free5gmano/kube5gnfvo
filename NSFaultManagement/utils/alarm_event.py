8d423dbad2707d0a45a360276f2a797fcd923c4d
chocolee1024 <chocolee1020@gmail.com>
2026-04-14 06:23:20 +0000

    告警完成版

--- NSFaultManagement/utils/alarm_event.py
+++ NSFaultManagement/utils/alarm_event.py
@@ -20,6 +20,13 @@ from datetime import datetime
 
 from utils.notification_management.kafka_notification import KafkaNotification
 
+# gNB / UE 是另一套 model，alarm 也要支援查它們
+try:
+    from GnbManagement.models import GnbInstance, UeInstance
+except ImportError:
+    GnbInstance = None
+    UeInstance = None
+
 
 class AlarmEvent(object):
     error_record = dict()
@@ -36,45 +43,78 @@ class AlarmEvent(object):
 
         return json.dumps(ns_instance_id), json.dumps(ns_instance_link)
 
-    def create_alarm(self, name: str, reason: str, message: str, is_container: bool):
+    def _find_instance_by_pod_name(self, name: str, is_container: bool):
+        """
+        Try to find a matching instance for a given pod name.
+        Returns (instance, instance_type) where instance_type is 'vnf', 'gnb', or 'ue'.
+        """
+        candidates = []
         if is_container:
-            # Try progressively shorter prefixes to match VnfInstance name.
-            # Pod name format varies: {vnf_name}-{rs_hash}-{pod_hash}
-            # but the number of '-' segments in the suffix is unpredictable.
+            # 容器 Pod name 格式: {deployment-name}-{rs-hash}-{pod-hash}
+            # 嘗試逐步縮短前綴
             pod_name_list = name.split('-')
-            vnf_instance = None
-            for i in range(1, min(len(pod_name_list), 4)):
+            for i in range(1, min(len(pod_name_list), 5)):
                 candidate = '-'.join(pod_name_list[:-i])
-                if not candidate:
-                    break
-                vnf_instance = VnfInstance.objects.filter(vnfInstanceName=candidate).last()
-                if vnf_instance:
-                    break
+                if candidate:
+                    candidates.append(candidate)
         else:
-            vnf_name = name[:-5]
-            vnf_instance = VnfInstance.objects.filter(vnfInstanceName=vnf_name).last()
+            candidates.append(name[:-5])
 
-        if vnf_instance:
-            ns_instance_id, ns_instance_link = self.managed_object(vnf_instance)
-            check = self._time_check(ns_instance_id, str(vnf_instance.id))
-            if check:
-                self.kafka_notification.notify(ns_instance_id, 'NS Instance({}) crashed'.format(ns_instance_id))
-                alarm = Alarm.objects.create(
-                    **{'managedObjectId': ns_instance_id,
-                       'probableCause': reason,
-                       'faultDetails': message})
+        for candidate in candidates:
+            # 1. 先找 VnfInstance (5GC)
+            vnf = VnfInstance.objects.filter(vnfInstanceName=candidate).last()
+            if vnf:
+                return vnf, 'vnf'
+            # 2. 找 GnbInstance
+            if GnbInstance is not None:
+                gnb = GnbInstance.objects.filter(gnbInstanceName=candidate).last()
+                if gnb:
+                    return gnb, 'gnb'
+            # 3. 找 UeInstance
+            if UeInstance is not None:
+                ue = UeInstance.objects.filter(ueInstanceName=candidate).last()
+                if ue:
+                    return ue, 'ue'
+        return None, None
 
-                AlarmLinks.objects.create(
-                    _links=alarm,
-                    **{'link_self': 'nsfm/v1/alarms/{}'.format(alarm.id),
-                       'objectInstance': ns_instance_link})
+    def create_alarm(self, name: str, reason: str, message: str, is_container: bool):
+        instance, instance_type = self._find_instance_by_pod_name(name, is_container)
+        if instance is None:
+            return
 
-                FaultyComponentInfo.objects.create(
-                    rootCauseFaultyComponent=alarm,
-                    **{'faultyVnfInstanceId': str(vnf_instance.id)}
-                )
+        if instance_type == 'vnf':
+            ns_instance_id, ns_instance_link = self.managed_object(instance)
+            faulty_id = str(instance.id)
+        else:
+            # gNB / UE 沒有 NsInstance 關聯，直接用 instance 自己當 managedObject
+            ns_instance_id = json.dumps([str(instance.id)])
+            ns_instance_link = json.dumps([f'/{instance_type}/v1/instances/{instance.id}'])
+            faulty_id = str(instance.id)
 
-                FaultyResourceInfo.objects.create(rootCauseFaultyResource=alarm)
+        check = self._time_check(ns_instance_id, faulty_id)
+        if not check:
+            return
+
+        self.kafka_notification.notify(
+            ns_instance_id,
+            '{} Instance({}) crashed'.format(instance_type.upper(), ns_instance_id),
+        )
+        alarm = Alarm.objects.create(
+            **{'managedObjectId': ns_instance_id,
+               'probableCause': reason,
+               'faultDetails': message})
+
+        AlarmLinks.objects.create(
+            _links=alarm,
+            **{'link_self': 'nsfm/v1/alarms/{}'.format(alarm.id),
+               'objectInstance': ns_instance_link})
+
+        FaultyComponentInfo.objects.create(
+            rootCauseFaultyComponent=alarm,
+            **{'faultyVnfInstanceId': faulty_id}
+        )
+
+        FaultyResourceInfo.objects.create(rootCauseFaultyResource=alarm)
 
     # 同一個 (ns_id, vnf_id) 在此秒數內重複告警會被擋掉
     COOLDOWN_SECONDS = 60
